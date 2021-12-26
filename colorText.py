c4ebb58f3a05db22f0d776302fab4b37e71003c6
R3CKhi-**75 <60217530+L45T500TH4CCK@users.noreply.github.com>
2021-12-26 20:04:18 +0300

    Update colorText.py

--- colorText.py
+++ colorText.py
@@ -1,11 +1,59 @@
 import os
 class colors():
+
+    # Colors
+    
     Lime = '\33[92m'
-
-    Red = '\033[91m'
-
-    Yellow = '\033[93m'
-
+    Red = '\33[91m'
+    Yellow = '\33[93m'       
+    Black  = '\33[30m'
+    
+    Green = '\33[32m'    
+    Blue   = '\33[34m'
+    Violet = '\33[35m'
+    Beige  = '\33[36m'
+    White = '\33[37m'
+    Grey    = '\33[90m'
+    
+    BG_Black = '\33[40m'
+    BG_Red = '\33[41m'
+    BG_Green  = '\33[42m'
+    BG_Yellow = '\33[43m'
+    BG_Blue   = '\33[44m'
+    BG_Violet = '\33[45m'
+    BG_Beige = '\33[46m'
+    BG_White = '\33[47m'
+        
+    Red2    = '\33[31m'    
+    Yellow2 = '\33[33m'
+    Blue2   = '\33[94m'
+    Violet2 = '\33[95m'
+    Beige2  = '\33[96m'
+    White2  = '\33[97m'
+    
+    BG_Grey    = '\33[100m'
+    BG_Red2    = '\33[101m'
+    BG_Green2  = '\33[102m'
+    BG_Yellow2 = '\33[103m'
+    BG_Blue2   = '\33[104m'
+    BG_Violet2 = '\33[105m'
+    BG_Beige2  = '\33[106m'
+    BG_White2  = '\33[107m'
+    
+    # Type
+    
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+    
+    
+    # End
     CD = '\033[0m'
     
 os.system("")
