ed1dc34fea9577d4e5ee69dace79aab34a593d24
R3CKhi-**75 <60217530+L45T500TH4CCK@users.noreply.github.com>
2021-12-26 18:14:26 +0300

    Add files via upload

---
+++ colorText.py
@@ -0,0 +1,12 @@
+import os
+class colors():
+    Lime = '\33[92m'
+
+    Red = '\033[91m'
+
+    Yellow = '\033[93m'
+
+    CD = '\033[0m'
+    
+os.system("")
+info = colors.Yellow + "[INFO]" + colors.CD
